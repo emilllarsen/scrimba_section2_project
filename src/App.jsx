f9a6c1885109c9889c0536270742cb52b5f18e3c
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-04 18:02:15 +0100

    one hardcoded card done. Using props soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Entry from "./components/Entry";
 
 export default function App() {
   return (
     <>
       <Header/>
+      <Entry/>
     </>
 
   );
