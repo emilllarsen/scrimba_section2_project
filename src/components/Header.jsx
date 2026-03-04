499d360a3a1fe82840d0cdd0d2f73f98502ad8a8
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-04 17:21:44 +0100

    feat: header with a background, and a globe icon with a simple text

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="../../public/images/world-globe-white-icon.webp" alt="globe icon" />
+            <span>my travel journal</span>
+        </header>
+    );
+}
