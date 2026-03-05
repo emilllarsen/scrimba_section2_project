e65f384b46bce19cb85eeb5060a6305b9fc3649b
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-05 10:28:17 +0100

    added props in Entry so its not that hard coded

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,24 +1,20 @@
-export default function Entry(prop) {
+export default function Entry(props) {
   return (
     <main className="entryMain">
       <figure className="imgFigure">
         <img
           className="entryImg"
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="Mountain image of mount fuji"
+          src={props.img}
+          alt={props.alt}
         />
       </figure>
       <section className="entrySectionInformation">
-        <img className="entryMarker" src="../../public/images/marker.png" alt="Marker icon" />
-        <span className="country">Japan</span>
-        <a className="googleMapsLink" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" target="_blank">View In Google Maps</a>
-        <h2 className="entryMountain">Mount Fuji</h2>
-        <h3 className="entryDate">12 Jan, 2021 - 24 Jan, 2021</h3>
-        <p className="entryPara">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <img className="entryMarker" src={props.marker} alt={props.markerText} />
+        <span className="country">{props.country}</span>
+        <a className="googleMapsLink" href={props.googleMapsLink} target="_blank">View In Google Maps</a>
+        <h2 className="entryMountain">{props.title}</h2>
+        <h3 className="entryDate">{props.date}</h3>
+        <p className="entryPara">{props.text}</p>
       </section>
     </main>
   );
